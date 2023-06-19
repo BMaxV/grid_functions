0d08bdec99ab0411615a94f8eaeae1dd91182952
max <bruno.m.voss@gmail.com>
2023-06-19 17:38:03 +0200

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name = "grid functions",
+    version = "0.10",
+    author = "Brumo Maximilian Voss",
+    author_email = "bruno.m.voss@gmail.com",
+    description = ("some simple functions to deal with grids"),
+    
+    packages=['grid_functions'],
+)
